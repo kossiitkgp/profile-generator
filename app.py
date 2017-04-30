5092dcaed4f60ac742d26ad8970d69eaf3809765
Dibya Das <dibyadascool@gmail.com>
2017-04-30 16:04:15 +0530

    pep8 fixes done

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
 from flask import Flask, redirect, url_for, request, render_template
 
-app = Flask(__name__)   
+app = Flask(__name__)
+
 
 @app.route('/<name>')
 def main(name):
-	return render_template('test.html',name = name)
+    return render_template('test.html', name=name)
 
 
-if __name__ == "__main__":							# This if block is to run the app on our local machine
-    app.run(host='localhost', port=3453,debug=True)
+if __name__ == "__main__":  # This is for local testin
+    app.run(host='localhost', port=3453, debug=True)
 
-
-# if __name__ == "__main__":  						#This piece of code will come in use when
-#     port = int(os.environ.get("PORT", 5000))    	#when the app is deployed on heroku and will replace the above if block
+# if __name__ == "__main__":  # This will come in use when
+#     port = int(os.environ.get("PORT", 5000))  # the app is deployed on heroku
 #     app.run(host='0.0.0.0', port=port)
