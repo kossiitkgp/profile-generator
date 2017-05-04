7855ef5111a14fd0e76c5563d0804ccfa7c681b6
aribis369 <aribis369@gmail.com>
2017-05-04 11:03:18 +0800

    added modules for getting a members repos and their links

--- app.py
+++ app.py
@@ -1,14 +1,35 @@
 from flask import Flask, redirect, url_for, request, render_template
+from urllib.request import urlopen
+import json
+
+repos=[]
+repolink=[]
+def repogen(user_name):
+    global repos
+    global repolink
+    httpob=urlopen("https://api.github.com/users/"+user_name+"/repos")
+    decob=httpob.read().decode("utf-8")
+    jsonob=json.loads(decob)
+    #print(jsonob)
+    for j in jsonob:
+        if str(j["fork"])=="False":
+            #print(j["full_name"]+"\n")
+            #print("https://www.github.com/"+j["full_name"])
+            repos.append(j["full_name"])
+            repolink.append("https://www.github.com/"+j["full_name"])
 
 app = Flask(__name__)
 
 
 @app.route('/<name>')
 def main(name):
+    global repos
+    global repolink
+    repogen(name)
     return render_template('test.html', name=name)
 
 
-if __name__ == "__main__":  # This is for local testin
+if __name__ == "__main__":  # This is for local testing
     app.run(host='localhost', port=3453, debug=True)
 
 # if __name__ == "__main__":  # This will come in use when
