b3454d49d8ebb7a75429b0041ae1db5be4311fb2
Dibya Das <dibyadascool@gmail.com>
2017-05-06 13:10:34 +0530

    test deploy

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, redirect, url_for, request, render_template
 from flask_mail import Mail, Message
+import os
+import json
 app = Flask(__name__)
 
 
@@ -11,21 +13,21 @@ def main(name):
 @app.route('/mail', methods=['POST'])
 def mail():
 	app.config['MAIL_SERVER']='smtp.gmail.com'
-	app.config['MAIL_PORT'] = 465
-	app.config['MAIL_USE_TLS'] = False
-	app.config['MAIL_USE_SSL']= True
-	app.config['MAIL_USERNAME'] = 'dibyadascool@gmail.com'
-	app.config['MAIL_PASSWORD'] = 'test'
+	app.config['MAIL_PORT'] = 587      #465 for SSL
+	app.config['MAIL_USE_TLS'] = True
+	app.config['MAIL_USE_SSL']=  False #True
+	app.config['MAIL_USERNAME'] = 'dibyadasiscool@gmail.com'
+	app.config['MAIL_PASSWORD'] = 'samsunghp'
 	mail = Mail(app)
 	form_data = request.form['msg']
-	msg = Message('Query',sender = 'dibyadascool@gmail.com', recipients = ['dibyadas998@gmail.com'])
+	msg = Message('Query',sender = 'dibyadasiscool@gmail.com', recipients = ['dibyadas998@gmail.com'])
 	msg.body = form_data
 	mail.send(msg)
-	return "Sent"
+	return ("sent",200,{'Access-Control-Allow-Origin':'*'})
 
-if __name__ == "__main__":  # This is for local testin
-    app.run(host='localhost', port=3453, debug=True)
+# if __name__ == "__main__":  # This is for local testin
+#     app.run(host='localhost', port=3453, debug=True)
 
-# if __name__ == "__main__":  # This will come in use when
-#     port = int(os.environ.get("PORT", 5000))  # the app is deployed on heroku
-#     app.run(host='0.0.0.0', port=port)
+if __name__ == "__main__":  # This will come in use when
+    port = int(os.environ.get("PORT", 5000))  # the app is deployed on heroku
+    app.run(host='0.0.0.0', port=port)
