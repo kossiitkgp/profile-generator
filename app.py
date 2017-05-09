9b57ca29e8e72e63c3db0a75cfd15ae8d4ffe8fb
Dibya Das <dibyadascool@gmail.com>
2017-05-09 23:53:58 +0530

    testing

--- app.py
+++ app.py
@@ -1,24 +1,27 @@
 from flask import Flask, redirect, url_for, request, render_template
-from urllib.request import urlopen
+import requests
 import json
 
 # global variables to store repo and repo links
 repos=[]
 repolink=[]
+image=""
 
 # module to generate repo and repo links
 def repogen(user_name):
     global repos
     global repolink
+    global image
     # getting contents of the api for a user
-    httpob=urlopen("https://api.github.com/users/"+user_name+"/repos")
+    httpob=requests.get("https://api.github.com/users/"+user_name+"/repos?per_page=100")
     # decoding the http object recieved
-    decob=httpob.read().decode("utf-8")
+    decob=httpob.content.decode("utf-8")
     # converting to json
     jsonob=json.loads(decob)
+    image = jsonob[0]['owner']['avatar_url']
     # appending only personal repos to the list
     for j in jsonob:
-        if str(j["fork"])=="False":
+        if j["fork"] != True:
             repos.append(j["full_name"])
             repolink.append("https://www.github.com/"+j["full_name"])
             
@@ -33,7 +36,11 @@ def main(name):
     # generating repo and repolink list before rendering the html file
     # list to be passed while rendering & some more credentials can be added 
     repogen(name)
-    return render_template('test.html', name=name)
+
+    x = render_template('temp.html', name=name ,image=image, repos=repos)
+    with open("templates/t2.html","w") as f:
+        f.write(x)
+    return "True"
 
 
 if __name__ == "__main__":  # This is for local testing
