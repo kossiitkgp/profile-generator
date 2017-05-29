72238ed3ee31718e304ac0a615ab68938e97230b
Dibya Das <dibyadascool@gmail.com>
2017-05-29 14:19:32 +0530

    mail sending part separated

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 from flask import Flask, redirect, url_for, request, render_template, Response
-from flask_mail import Mail, Message
 import os
 import json
 import requests
-from multiprocessing import Pool
 
 # global variables to store repo and repo links
 repos = []
@@ -46,59 +44,8 @@ def main(name):
     owner_data = json.loads(owner.content.decode("utf-8"))
     x = render_template('temp.html', name=name, image=image,
                         repo_dict=repo_dict, owner_data=owner_data)
-    with open("templates/t2.html", "w") as f:
-        f.write(x)
     headers = {"Access-Control-Allow-Origin": "*", 'Content-Type': 'text/html'}
     return Response(x, 200, headers=headers)
-
-
-def send_mail(form_msg, form_name, form_email):
-    print("inside async")
-    app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-    app.config['MAIL_PORT'] = 587  # 465 for SSL
-    app.config['MAIL_USE_TLS'] = True
-    app.config['MAIL_USE_SSL'] = False  # True
-    app.config['MAIL_USERNAME'] = 'dibyadasiscool@gmail.com'
-    app.config['MAIL_PASSWORD'] = 'samsunghp'
-    print("all config done")
-    mail = Mail(app)
-    print("init mail app")
-    form_data = form_msg  # request.form['msg']
-    print("message taken")
-    msg = Message('Query', sender='dibyadasiscool@gmail.com',
-                  recipients=['dibyadas998@gmail.com'])
-    print("message init")
-    msg.body = "Query sent by:- " + form_name + "\n" + \
-        form_data + "\n" + "Email-ID is:- " + form_email
-    print("body made")
-    print("going to send the msg")
-    mail.send(msg)
-    print("sent")
-    return None
-
-
-@app.route('/mail', methods=['POST'])
-def mail():
-    pool = Pool(processes=10)
-    r = pool.apply_async(
-        send_mail, [request.form['msg'],
-                    request.form['name'],
-                    request.form['email']])
-    # e =send_mail(request.form['msg'],request.form['name'],request.form['email'])
-    return ("sent", 200, {'Access-Control-Allow-Origin': '*'})
-
-
-@app.route('/captcha',methods=['POST'])
-def captcha():
-    t = json.loads(request.data.decode("utf-8"))
-    key = t['key']
-    gcaptcha = t['gcaptcha']
-    r = requests.post("https://www.google.com/recaptcha/api/siteverify",data = { 'secret' : key, 'response' : gcaptcha } )
-    print(r.content.decode('utf-8'))
-    if json.loads(r.content.decode('utf-8'))['success'] == True:
-        return ("sent", 200, {'Access-Control-Allow-Origin': '*'})
-    else:
-        return ("Not sent", 400, {'Access-Control-Allow-Origin': '*'})
 
 
 # if __name__ == "__main__":  # This is for local testin
