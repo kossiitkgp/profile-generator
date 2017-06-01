ba65118867a512c58ade0a8e34519fdf6ec0f25c
Dibya Das <dibyadascool@gmail.com>
2017-06-01 18:21:52 +0530

    added support for about us card generation

--- app.py
+++ app.py
@@ -2,15 +2,15 @@ from flask import Flask, redirect, url_for, request, render_template, Response
 import os
 import json
 import requests
+from collections import OrderedDict
 
 app = Flask(__name__)
 
 
 @app.route('/<name>')
-def main(name, position=None):
+def profile(name, position=None):
     url = "https://api.github.com/graphql"
-    # headers = {"Authorization":os.environ[OUATH_KEY]}
-    headers = {"Authorization": "Basic ZGlieWFkYXM6bmFuZGFEQVNAOTk="}
+    headers = {"Authorization": os.environ[OUATH_KEY]}
     query = json.dumps({"query": "query{user(login: \"" + name +
                         "\") { name email avatarUrl url bio websiteUrl"
                         " pinnedRepositories(first: 6) "
@@ -35,8 +35,28 @@ def main(name, position=None):
                         blog=blog,
                         position=position
                         )
-    headers = {"Access-Control-Allow-Origin": "*", 'Content-Type': 'text/html'}
-    with open("templates/" + name + ".html", "w") as f:
+    with open("pages/" + name + ".html", "w") as f:
+        f.write(x)
+    return "done"
+
+
+def card(name, position=None):
+    print("in card")
+    url = "https://api.github.com/graphql"
+    headers = {"Authorization": os.environ[OUATH_KEY]}
+    query = json.dumps({"query": "query{user(login: \"" + name +
+                        "\") { name avatarUrl"
+                        " } }"})
+    r = requests.post(url, headers=headers, data=query)
+    data_dict = json.loads(r.content.decode("utf-8"), sorted)
+    image = data_dict['data']['user']['avatarUrl']
+    full_name = data_dict['data']['user']['name']
+    x = render_template('cards.tmpl', image=image,
+                        full_name=full_name,
+                        position=position,
+                        name=name
+                        )
+    with open("cards/all.html", "a") as f:
         f.write(x)
     return "done"
 
@@ -45,18 +65,43 @@ def main(name, position=None):
 @app.route('/generate-<id>')    # for generating the profile of a single person
 def generate(id=None):          # given their github id
     if id is not None:
-        main(id)
+        profile(id, position="Member")
     else:
         with open("data.json") as json_data_file:
             json_data = json.load(json_data_file)
             for i in json_data:
                 for j in json_data[i]:
                     try:
-                        main(j, i)
+                        profile(j, i)
                     except Exception:
-                        print("error")
-                        print("id " + j)
-    return "completed"
+                        print("Error generating profile for :- ")
+                        print("ID - " + j)
+    return "Completed"
+
+
+@app.route('/card')
+@app.route('/card-<id>')
+def generate_card(id=None):
+    if id is not None:
+        card(id, position="Member")
+    else:
+        try:
+            os.remove("cards/all.html")
+        except Exception:
+            pass
+        os.system("touch cards/all.html")
+        with open("data.json") as json_data_file:
+            json_data = json.load(json_data_file,
+                                  object_pairs_hook=OrderedDict
+                                  )
+            for i in json_data:
+                for j in json_data[i]:
+                    try:
+                        card(j, i)
+                    except Exception:
+                        print("Error generating profile for :- ")
+                        print("ID - " + j)
+    return "Completed"
 
 
 if __name__ == "__main__":
